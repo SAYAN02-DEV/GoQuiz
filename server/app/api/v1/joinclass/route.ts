52814522c1ab41358ba0e1d3447b9399b6362c9a
Sayan Manna <sayan2003.dev@gmail.com>
2026-03-08 00:18:11 +0000

    feat:join class api

---
+++ server/app/api/v1/joinclass/route.ts
@@ -0,0 +1,44 @@
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { getStudentSession } from "@/app/lib/auth";
+import { JoinClass } from "@/app/types";
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+const prisma = new PrismaClient({ adapter });
+
+export async function POST(request: NextRequest) {
+    const session = await getStudentSession();
+    if(!session){
+        return NextResponse.json(
+            {error: "Unauthorized"},
+            {status: 401}
+        )
+    }
+    const {classId, secretKey} = await request.json() as JoinClass;
+    if(!secretKey){
+        return NextResponse.json(
+            {error: "secret key is required"},
+            {status: 400}
+        )
+    }
+    const classInfo = await prisma.class.findFirst({where: {class_id:classId}});
+    if(!classInfo){
+        return NextResponse.json(
+            {error: "class not found"},
+            {status: 401}
+        )
+    }
+    else if(classInfo?.secret_key === secretKey){
+        const newAdmission = await prisma.class_Admission.create({
+            data:{
+                student_id: session.student_id,
+                class_id: classId
+            }
+        });
+        return NextResponse.json(
+            {classAdmissionId: newAdmission.admission_id},
+            {status: 200}
+        )
+    }
+}
--- server/app/types/index.ts
+++ server/app/types/index.ts
@@ -10,3 +10,8 @@ export interface CreateQuizRequest {
     dueDate: Date;
     isResultOut: boolean
 }
+
+export interface JoinClass{
+    classId: number
+    secretKey: String
+}
